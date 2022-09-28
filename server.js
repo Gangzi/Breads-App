28276b31da11e2f7563b04a3f82afca496ca5eb8
Gang Cao <gangcao@Gangs-MacBook-Pro.local>
2022-09-28 19:05:48 -0400

    delete route complete

--- server.js
+++ server.js
@@ -1,23 +1,34 @@
+
 const express = require('express')
+const { createEngine } = require('express-react-views')
 const app = express()
+const methodOverride = require('method-override')
 // CONFIGURATION
 require('dotenv').config()
 const PORT = process.env.PORT
 console.log(PORT)
 
-// ROUTES
-app.get('/', (req, res) => {
-  res.send('Welcome to an Awesome App about Breads')
-})
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+app.use(express.static('public'))
+app.use(express.urlencoded({ extended: true }))
+app.use(methodOverride('_method'))
+
+// ROUTES
+app.get('/', (req, res) => {
+  res.send('Welcome to an Awesome App about Breads')
+})
 
 // Breads
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
 
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
 
 app.listen(PORT, () => {
   console.log('listening on port', PORT)
